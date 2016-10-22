3a918b7f0c8e039ee854543ccdfc71ede0ac5609
Costica puntaru <ticaasd@gmail.com>
2016-10-22 16:59:39 +0300

    semafor updater

--- semafoare-server/semafoare.js
+++ semafoare-server/semafoare.js
@@ -1,4 +1,20 @@
 const semafoareDb = require('./semafoare-db.json');
+const UPDATE_INTERVAL = 1000;
+
+
+setInterval(function () {
+    for (var i = 0; i < semafoareDb.length; i++) {
+        var semafor = semafoareDb[i];
+        if (semafor.nextChange <= UPDATE_INTERVAL) {
+            semafor.northSouth = !semafor.northSouth;
+            semafor.eastWest = !semafor.northSouth;
+            semafor.nextChange = semafor.timeInterval;
+        } else {
+            semafor.nextChange -= UPDATE_INTERVAL;
+        }
+    }
+}, UPDATE_INTERVAL);
+
 
 module.exports = {
     data: semafoareDb
