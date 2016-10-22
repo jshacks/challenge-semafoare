9d2065781f734d66b94f31a22e21314be5e73376
Adrian Neatu <adrian.neatu@everymatrix.com>
2016-10-22 16:08:02 +0300

    add more server stuff

--- semafoare-server/index.js
+++ semafoare-server/index.js
@@ -1,63 +1,62 @@
 const express = require('express');
 const app = express();
+const semafoareDb = require('./semafoare').data;
 
-app.get('/', function (req, res) {
+app.post('/get-semafoare', function (req, res) {
 
-    // GOOD points
+    const startLat = req.query.start_lat;
+    const startLng = req.query.start_lng;
+    const endLat = req.query.end_lat;
+    const endLng = req.query.end_lng;
+
     const point1 = {
-        x: 24.5050602,
-        y: 43.7764338
+        lng: startLng,
+        lat: startLat
     };
     const point2 = {
-        x: 24.5064149,
-        y: 43.7804849
+        lng: endLng,
+        lat: endLat
     };
 
-    // BAD points
-    // const point1 = {
-    //     x: 24.508422,
-    //     y: 43.779596
-    // };
-    // const point2 = {
-    //     x: 24.5083147,
-    //     y: 43.7806069
-    // };
+    const messages = [];
 
-    const currPoint = {
-        x: 24.5050602,
-        y: 43.7764338
-    };
+    semafoareDb.map((semafor, i) => {
+        const currPoint = semafor;
 
-    const dxc = currPoint.x - point1.x;
-    const dyc = currPoint.y - point1.y;
+        const dxc = currPoint.lng - point1.lng;
+        const dyc = currPoint.lat - point1.lat;
 
-    const dxl = point2.x - point1.x;
-    const dyl = point2.y - point1.y;
+        const dxl = point2.lng - point1.lng;
+        const dyl = point2.lat - point1.lat;
 
-    const cross = dxc * dyl - dyc * dxl;
+        const cross = dxc * dyl - dyc * dxl;
 
-    let msg = '';
+        if (cross === 0) {
+            messages[i] = 'On the same line.';
 
-    if (cross === 0) {
-        msg = 'On the same line.';
-    }
-    else {
-        msg = 'Not on the same line.'
-    }
+            let response = '';
+            if (Math.abs(dxl) >= Math.abs(dyl)) {
+                response =  dxl > 0 ?
+                point1.lng <= currPoint.lng && currPoint.lng <= point2.lng :
+                point2.lng <= currPoint.lng && currPoint.lng <= point1.lng;
+            }
+            else {
+                response = dyl > 0 ?
+                point1.lat <= currPoint.lat && currPoint.lat <= point2.lat :
+                point2.lat <= currPoint.lat && currPoint.lat <= point1.lat;
+            }
 
-    let response = '';
-    if (Math.abs(dxl) >= Math.abs(dyl)) {
-        response =  dxl > 0 ?
-        point1.x <= currPoint.x && currPoint.x <= point2.x :
-        point2.x <= currPoint.x && currPoint.x <= point1.x;
-    }
-    else {
-        response = dyl > 0 ?
-        point1.y <= currPoint.y && currPoint.y <= point2.y :
-        point2.y <= currPoint.y && currPoint.y <= point1.y;
-    }
+            if (response) {
+                messages[i] += ` & between the points of ${semafor.name}`;
+            }
+        }
+        else {
+            messages[i] = `Not on the same line with ${semafor.name}`;
+        }
 
-    res.send(`${msg}: ${response}`);
+    });
+
+    res.send(JSON.stringify(messages));
 });
 
 app.listen(3000, function () {
---
+++ semafoare-server/semafoare.js
@@ -0,0 +1,5 @@
+const semafoareDb = require('./semafoare-db.json');
+
+module.exports = {
+    data: semafoareDb
+};
