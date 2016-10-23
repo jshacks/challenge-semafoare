ad39c5e2a936f0f1a398fdc1e13ff1012df46232
kinko_2007 <kinko_2007@users.noreply.github.com>
2016-10-23 13:01:26 +0300

    Styling MVP

--- google-maps-lights/src/app/app.component.ts
+++ google-maps-lights/src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 
-import 'bootstrap/dist/css/bootstrap.min.css'
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Google Maps(with lights timing)!';
+  // title = 'Google Maps(with lights timing)!';
 }
